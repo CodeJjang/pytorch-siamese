42f956284d9cd87fe96a9f6872b1ecf0ca73beb0
aviad_m <aviad.moreshet@traxretail.com>
2020-08-16 22:57:18 +0300

    Organized BatchHard code

--- src/sampling/BatchHard.py
+++ src/sampling/BatchHard.py
@@ -1,30 +1,38 @@
 import torch
 import numpy as np
 
+
 class BatchHard:
 
-    def __call__(self, embeds, labels):
-        embeds = torch.stack([embed for embed in embeds]).reshape(-1, 2)
+    def __call__(self, embeddings, labels):
+        # Unroll embeddings and labels
+        embeddings = torch.stack([embed for embed in embeddings]).reshape(-1, 2)
         labels = labels.reshape(-1)
 
-        dist_mtx = self._pdist_torch(embeds, embeds).detach().cpu().numpy()
+        batch_size = labels.shape[0]
+        distances = self._calc_dist(embeddings, embeddings).detach().cpu().numpy()
         labels = labels.contiguous().cpu().numpy().reshape((-1, 1))
-        num = labels.shape[0]
-        dia_inds = np.diag_indices(num)
-        lb_eqs = labels == labels.T
-        lb_eqs[dia_inds] = False
-        dist_same = dist_mtx.copy()
-        dist_same[lb_eqs == False] = -np.inf
-        pos_idxs = np.argmax(dist_same, axis=1)
-        dist_diff = dist_mtx.copy()
-        lb_eqs[dia_inds] = True
-        dist_diff[lb_eqs == True] = np.inf
-        neg_idxs = np.argmin(dist_diff, axis=1)
-        pos = embeds[pos_idxs].contiguous().view(num, -1)
-        neg = embeds[neg_idxs].contiguous().view(num, -1)
-        return embeds, pos, neg
 
-    def _pdist_torch(self, emb1, emb2):
+        diag_indices = np.diag_indices(batch_size)
+
+        # Calc positive indices
+        label_eq_mask = labels == labels.T
+        label_eq_mask[diag_indices] = False
+        dist_same = distances.copy()
+        dist_same[label_eq_mask == False] = -np.inf
+        positive_indices = np.argmax(dist_same, axis=1)
+
+        # Calc negative indices
+        dist_diff = distances.copy()
+        label_eq_mask[diag_indices] = True
+        dist_diff[label_eq_mask == True] = np.inf
+        negative_indices = np.argmin(dist_diff, axis=1)
+
+        pos = embeddings[positive_indices].contiguous().view(batch_size, -1)
+        neg = embeddings[negative_indices].contiguous().view(batch_size, -1)
+        return embeddings, pos, neg
+
+    def _calc_dist(self, emb1, emb2):
         '''
         compute the eucilidean distance matrix between embeddings1 and embeddings2
         using gpu
@@ -32,7 +40,7 @@ class BatchHard:
         m, n = emb1.shape[0], emb2.shape[0]
         emb1_pow = torch.pow(emb1, 2).sum(dim=1, keepdim=True).expand(m, n)
         emb2_pow = torch.pow(emb2, 2).sum(dim=1, keepdim=True).expand(n, m).t()
-        dist_mtx = emb1_pow + emb2_pow
-        dist_mtx = dist_mtx.addmm_(1, -2, emb1, emb2.t())
-        dist_mtx = dist_mtx.clamp(min=1e-12).sqrt()
-        return dist_mtx
+        distances = emb1_pow + emb2_pow
+        distances = distances.addmm_(emb1, emb2.t(), beta=1, alpha=-2)
+        distances = distances.clamp(min=1e-12).sqrt()
+        return distances
